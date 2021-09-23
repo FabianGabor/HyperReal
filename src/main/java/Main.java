566c9edb883f4beaa3988bd1268f541dab4e6b2f
Fábián Gábor <info@fabiangabor.com>
2021-09-23 19:30:49 +0200

    Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		HyperInteger hyperInteger1 = new HyperInteger("12");
+		HyperInteger hyperInteger2 = new HyperInteger("34");
+
+		System.out.println("hyperInteger = " + hyperInteger1);
+		System.out.println("hyperInteger = " + hyperInteger2);
+	}
+}
