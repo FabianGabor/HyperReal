70b07301deca00915a3bd1123d63369948392281
Fábián Gábor <info@fabiangabor.com>
2021-09-24 18:23:50 +0200

    substract()

--- src/main/java/HyperInteger.java
+++ src/main/java/HyperInteger.java
@@ -84,6 +84,82 @@ public class HyperInteger implements Comparable<HyperInteger> {
 		return sum.toString();
 	}
 
+	public HyperInteger substract(HyperInteger number2) {
+		if (this.sign >= 0 && number2.sign >= 0) {
+			return substract(this, number2);
+		}
+		/*
+		if (this.sign >= 0 && number2.sign < 0 || this.sign < 0 && number2.sign >= 0)
+			return new HyperInteger(substract(digits, number2.digits));
+		if (this.sign < 0 && number2.sign < 0)
+			return new HyperInteger(add(digits, number2.digits), -1);
+
+		 */
+		return null;
+	}
+
+	private HyperInteger substract(HyperInteger number1, HyperInteger number2) {
+		HyperInteger diff = null;
+
+		if (number1.compareTo(number2) < 0) {
+			swap(number1, number2);
+			diff = new HyperInteger(substract(number1.digits, number2.digits));
+			diff.sign = -1;
+			swap(number1, number2);
+		}
+
+		return diff;
+	}
+
+	private String substract(byte[] number1, byte[] number2) {
+		StringBuilder diff = new StringBuilder();
+
+		reverse(number1);
+		reverse(number2);
+
+		int i = 0, j = 0;
+		int carry = 0;
+
+		do {
+			int localDiff = 0;
+			localDiff -= carry;
+
+			if (i < number1.length) localDiff += number1[i];
+			if (j < number2.length) localDiff -= number2[j];
+
+			i++;
+			j++;
+
+			if (localDiff < 0) {
+				localDiff += 10;
+				carry = 1;
+			} else {
+				carry = 0;
+			}
+
+			diff.append(localDiff);
+		} while (carry > 0 || i < number1.length || j < number2.length);
+
+		reverse(number1);
+		reverse(number2);
+		diff.reverse();
+		stripLeadingZeros(diff);
+
+		return diff.toString();
+	}
+
+	private void swap(HyperInteger number1, HyperInteger number2) {
+		byte[] tmp = number1.digits;
+		number1.digits = number2.digits;
+		number2.digits = tmp;
+	}
+
+	private void stripLeadingZeros(StringBuilder diff) {
+		while (diff.charAt(0) == '0') {
+			diff.deleteCharAt(0);
+		}
+	}
+
 	private void reverse(byte[] num) {
 		byte tmp;
 		for (int i = 0; i < num.length / 2; i++) {
