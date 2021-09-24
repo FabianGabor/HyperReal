3adf2a11e7c2cdad081d72ee9ca79abb491f25b7
Fábián Gábor <info@fabiangabor.com>
2021-09-24 22:48:52 +0200

    Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,17 @@
-import java.util.ArrayList;
-
 public class Main {
 
 	public static void main(String[] args) {
-		HyperInteger hyperInteger1 = new HyperInteger("12");
-		HyperInteger hyperInteger2 = new HyperInteger("34");
+		HyperInteger hyperInteger1 = new HyperInteger(String.valueOf(1));
+		HyperInteger hyperInteger2 = new HyperInteger(String.valueOf(-10));
+		HyperInteger sum = hyperInteger1.add(hyperInteger2);
+		HyperInteger sub = hyperInteger1.substract(hyperInteger2);
 
-		System.out.println("hyperInteger = " + hyperInteger1);
-		System.out.println("hyperInteger = " + hyperInteger2);
+		System.out.println("hyperInteger1 = " + hyperInteger1);
+		System.out.println("hyperInteger2 = " + hyperInteger2);
+		System.out.println("sum = " + sum);
+
+		System.out.println("hyperInteger1 = " + hyperInteger1);
+		System.out.println("hyperInteger2 = " + hyperInteger2);
+		System.out.println(hyperInteger1 + " - " + hyperInteger2 + " = " + sub);
 	}
 }
