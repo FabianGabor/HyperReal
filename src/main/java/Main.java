79e6fd9e0a5aa4811ac8187f1c6ba4e66e3cd9a3
Fábián Gábor <info@fabiangabor.com>
2021-09-26 16:46:48 +0200

    multiply

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
+import com.fabiangabor.hyperreal.HyperInteger;
+
 public class Main {
 
 	public static void main(String[] args) {
-		HyperInteger hyperInteger1 = new HyperInteger(String.valueOf(1));
-		HyperInteger hyperInteger2 = new HyperInteger(String.valueOf(-10));
-		HyperInteger sum = hyperInteger1.add(hyperInteger2);
+		HyperInteger hyperInteger1 = new HyperInteger(String.valueOf(89));
+		HyperInteger hyperInteger2 = new HyperInteger(String.valueOf(89));
+/*		HyperInteger sum = hyperInteger1.add(hyperInteger2);
 		HyperInteger sub = hyperInteger1.substract(hyperInteger2);
 
 		System.out.println("hyperInteger1 = " + hyperInteger1);
@@ -13,5 +15,7 @@ public class Main {
 		System.out.println("hyperInteger1 = " + hyperInteger1);
 		System.out.println("hyperInteger2 = " + hyperInteger2);
 		System.out.println(hyperInteger1 + " - " + hyperInteger2 + " = " + sub);
+ */
+		System.out.println(hyperInteger1 + " * " + hyperInteger2 + " = " + hyperInteger1.multiply(hyperInteger2));
 	}
 }
