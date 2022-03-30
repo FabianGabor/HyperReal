626f1d288102b04940823e51b227c898409fa09c
Fábián Gábor <gabor_fabian1@epam.com>
2022-03-30 20:26:34 +0200

    Operation interface

---
+++ src/main/java/com/fabiangabor/hyperreal/operation/Operation.java
@@ -0,0 +1,13 @@
+/*
+ * Fábián Gábor
+ * Copyright (c) 2022.
+ * https://github.com/FabianGabor
+ */
+
+package com.fabiangabor.hyperreal.operation;
+
+import com.fabiangabor.hyperreal.HyperInteger;
+
+public interface Operation {
+    HyperInteger execute(HyperInteger number1, HyperInteger number2);
+}
