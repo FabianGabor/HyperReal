6472c31dc5ef77caa876a10d17f13fe4a16026ec
Fábián Gábor <gabor_fabian1@epam.com>
2022-03-30 13:43:17 +0200

    unit tests

--- src/test/java/TestHyperInteger.java
+++ src/test/java/TestHyperInteger.java
@@ -6,193 +6,263 @@
 
 import com.fabiangabor.hyperreal.HyperInteger;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.math.BigInteger;
-import java.util.Random;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+class TestHyperInteger {
 
-public class TestHyperInteger {
-	@Test
-	void testZero() {
-		assertEquals("0", new HyperInteger("0").toString());
-		assertEquals("0", new HyperInteger("00").toString());
-		assertEquals("0", new HyperInteger("+0").toString());
-		assertEquals("0", new HyperInteger("-0").toString());
-		assertEquals("0", new HyperInteger("+++000").toString());
-		assertEquals(0, new HyperInteger("00").sign);
-	}
+    HyperInteger underTest;
 
-	@Test
-	void testPositive() {
-		HyperInteger hyperInteger1 = new HyperInteger("1");
-		assertEquals("1", hyperInteger1.toString());
-		assertEquals(1, hyperInteger1.sign);
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "00", "+0", "-0", "++0", "--0", "+-0"})
+    void zeroShouldBeZero(String arg) {
+        final String EXPECTED = "0";
+        underTest = new HyperInteger(arg);
 
-		HyperInteger hyperInteger2 = new HyperInteger("987654321098765432109876543210987654321098765432109876543210");
-		assertEquals("987654321098765432109876543210987654321098765432109876543210", hyperInteger2.toString());
-		assertEquals(1, hyperInteger2.sign);
-	}
+        assertEquals(EXPECTED, underTest.toString());
+    }
 
-	@Test
-	void testNegative() {
-		HyperInteger hyperInteger1 = new HyperInteger("-1");
-		assertEquals("-1", hyperInteger1.toString());
-		assertEquals(-1, hyperInteger1.sign);
+    @Test
+    void numberShouldEqualToItsValue() {
+        final String EXPECTED = "123";
+        underTest = new HyperInteger(EXPECTED);
 
-		HyperInteger hyperInteger2 = new HyperInteger("-987654321098765432109876543210987654321098765432109876543210");
-		assertEquals("-987654321098765432109876543210987654321098765432109876543210", hyperInteger2.toString());
-		assertEquals(-1, hyperInteger2.sign);
-	}
+        assertEquals(EXPECTED, underTest.toString());
+    }
 
-	@Test
-	void compareTo() {
-		assertEquals(0, new HyperInteger("-1").compareTo(new HyperInteger("-1")));
-		assertEquals(0, new HyperInteger("0").compareTo(new HyperInteger("0")));
-		assertEquals(0, new HyperInteger("1").compareTo(new HyperInteger("1")));
+    @Test
+    void bigNumberShouldEqualToItsValue() {
+        final String EXPECTED = "987654321098765432109876543210987654321098765432109876543210";
+        underTest = new HyperInteger(EXPECTED);
 
-		assertEquals(1, new HyperInteger("1").compareTo(new HyperInteger("-1")));
-		assertEquals(1, new HyperInteger("1").compareTo(new HyperInteger("0")));
-		assertEquals(1, new HyperInteger("-2").compareTo(new HyperInteger("-3")));
-		assertEquals(1, new HyperInteger("20").compareTo(new HyperInteger("11")));
-	}
+        assertEquals(EXPECTED, underTest.toString());
+    }
 
-	@Test
-	void addTwoPositiveNumbers() {
-		HyperInteger hyperInteger1 = new HyperInteger("0");
-		HyperInteger hyperInteger2 = new HyperInteger("2");
-		HyperInteger hyperIntegerSum = hyperInteger1.add(hyperInteger2);
+    @Test
+    void positiveNumberSignShouldBeOne() {
+        final int EXPECTED = 1;
+        underTest = new HyperInteger("+123");
 
-		assertEquals("2", hyperIntegerSum.toString());
-		assertEquals(1, hyperIntegerSum.sign);
-	}
+        assertEquals(EXPECTED, underTest.sign);
+    }
 
-	@Test
-	void addTwoBigPositiveNumbers() {
-		HyperInteger hyperInteger1 = new HyperInteger("98765432109876543210");
-		HyperInteger hyperInteger2 = new HyperInteger("12345678901234567890");
-		HyperInteger hyperIntegerSum = hyperInteger1.add(hyperInteger2);
+    @Test
+    void negativeNumberSignShouldBeMinusOne() {
+        final int EXPECTED = -1;
+        underTest = new HyperInteger("-123");
 
-		assertEquals("111111111011111111100", hyperIntegerSum.toString());
-		assertEquals(1, hyperIntegerSum.sign);
-	}
+        assertEquals(EXPECTED, underTest.sign);
+    }
 
-	@Test
-	void addTwoNegativeNumbers() {
-		HyperInteger hyperInteger1 = new HyperInteger("-2");
-		HyperInteger hyperInteger2 = new HyperInteger("-5");
-		HyperInteger hyperIntegerSum = hyperInteger1.add(hyperInteger2);
+    @Test
+    void multiplePositiveSignShouldBeOne() {
+        final int EXPECTED = 1;
+        underTest = new HyperInteger("++123");
 
-		assertEquals("-7", hyperIntegerSum.toString());
-		assertEquals(-1, hyperIntegerSum.sign);
-	}
+        assertEquals(EXPECTED, underTest.sign);
+    }
 
-	@Test
-	void addTwoBigNegativeNumbers() {
-		HyperInteger hyperInteger1 = new HyperInteger("-98765432109876543210");
-		HyperInteger hyperInteger2 = new HyperInteger("-12345678901234567890");
-		HyperInteger hyperIntegerSum = hyperInteger1.add(hyperInteger2);
+    @Test
+    void multipleNegativeSignShouldBeMinusOne() {
+        final int EXPECTED = -1;
+        underTest = new HyperInteger("--123");
 
-		assertEquals("-111111111011111111100", hyperIntegerSum.toString());
-		assertEquals(-1, hyperIntegerSum.sign);
-	}
+        assertEquals(EXPECTED, underTest.sign);
+    }
 
-	@Test
-	void addTwoNumbers() {
-		for (int i = -100; i <= 100; i++) // +/- 10000-nél már több perc lehet a futásidő!
-			for (int j = -100; j <= 100; j++) {
-				assertEquals(String.valueOf(i + j), new HyperInteger(String.valueOf(i)).add(new HyperInteger(String.valueOf(j))).toString());
-			}
-	}
+    @Test
+    void compareToShouldReturnZeroWhenNumbersAreEqual() {
+        final int EXPECTED = 0;
+        underTest = new HyperInteger("123");
+        HyperInteger underTest2 = new HyperInteger("123");
 
-	@Test
-	void substractTwoNumbers() {
-		for (int i = -100; i <= 100; i++) // +/- 10000-nél már több perc lehet a futásidő!
-			for (int j = -100; j <= 100; j++)
-				assertEquals(String.valueOf(i - j), new HyperInteger(String.valueOf(i)).substract(new HyperInteger(String.valueOf(j))).toString());
-	}
+        assertEquals(EXPECTED, underTest.compareTo(underTest2));
+    }
 
-	@Test
-	void multiply() {
-		for (int i = -100; i <= 100; i++) // +/- 10000-nél már több perc lehet a futásidő!
-			for (int j = -100; j <= 100; j++) {
-				//System.out.println(i + " * " + j + " = " + i * j);
-				assertEquals(String.valueOf(i * j), new HyperInteger(String.valueOf(i)).multiply(new HyperInteger(String.valueOf(j))).toString());
-			}
-	}
+    @Test
+    void compareToShouldReturnOneWhenFirstNumberIsBigger() {
+        final int EXPECTED = 1;
+        underTest = new HyperInteger("1234");
+        HyperInteger underTest2 = new HyperInteger("123");
 
-	@Test
-	void divide() throws Exception {
-		for (int i = -100; i <= 100; i++) // +/- 10000-nél már több perc lehet a futásidő!
-			for (int j = -100; j <= 100; j++) {
-				if (j != 0) {
-					assertEquals(String.valueOf(i / j), new HyperInteger(String.valueOf(i)).divide(new HyperInteger(String.valueOf(j))).toString());
-				}
-			}
-	}
+        assertEquals(EXPECTED, underTest.compareTo(underTest2));
+    }
 
+    @Test
+    void compareToShouldReturnMinusOneWhenSecondNumberIsBigger() {
+        final int EXPECTED = -1;
+        underTest = new HyperInteger("123");
+        HyperInteger underTest2 = new HyperInteger("1234");
 
-	void addTwoBigNumbers() {
-		String aString = "", bString = "";
-		Random random = new Random();
+        assertEquals(EXPECTED, underTest.compareTo(underTest2));
+    }
 
-		for (int i = 0; i < random.nextInt(1000000) + 1; i++)
-			aString += random.nextInt(10);
+    @Test
+    void addShouldReturnSum() {
+        final String EXPECTED = "124";
+        HyperInteger number1 = new HyperInteger("123");
+        HyperInteger number2 = new HyperInteger("1");
+        underTest = number1.add(number2);
 
-		for (int i = 0; i < random.nextInt(1000000) + 1; i++)
-			bString += random.nextInt(10);
+        assertEquals(EXPECTED, underTest.toString());
+    }
 
-		BigInteger sum = new BigInteger(aString).add(new BigInteger(bString));
-		assertEquals(sum.toString(), new HyperInteger(aString).add(new HyperInteger(bString)).toString());
-	}
+    @Test
+    void addShouldReturnSumWhenAddingBigNumbers() {
+        final String EXPECTED = "111111111011111111100";
+        HyperInteger number1 = new HyperInteger("98765432109876543210");
+        HyperInteger number2 = new HyperInteger("12345678901234567890");
+        underTest = number1.add(number2);
 
-	@Test
-	void addTwoBigNumbersLoop() {
-		for (int i = 0; i < 1000; i++)
-			addTwoBigNumbers();
-	}
+        assertEquals(EXPECTED, underTest.toString());
+    }
 
-	void substractTwoBigNumbers() {
-		String aString = "", bString = "";
-		Random random = new Random();
+    @Test
+    void addShouldReturnSumWhenAddingNegativeNumbers() {
+        final String EXPECTED = "-7";
+        HyperInteger number1 = new HyperInteger("-2");
+        HyperInteger number2 = new HyperInteger("-5");
+        underTest = number1.add(number2);
 
-		for (int i = 0; i < random.nextInt(1000000) + 1; i++)
-			aString += random.nextInt(10);
+        assertEquals(EXPECTED, underTest.toString());
+    }
 
-		for (int i = 0; i < random.nextInt(1000000) + 1; i++)
-			bString += random.nextInt(10);
+    @Test
+    void addTwoBigNegativeNumbers() {
+        final String EXPECTED = "-111111111011111111100";
+        HyperInteger number1 = new HyperInteger("-98765432109876543210");
+        HyperInteger number2 = new HyperInteger("-12345678901234567890");
+        underTest = number1.add(number2);
 
-		BigInteger sum = new BigInteger(aString).subtract(new BigInteger(bString));
-		assertEquals(sum.toString(), new HyperInteger(aString).substract(new HyperInteger(bString)).toString());
-	}
-
-	@Test
-	void substractTwoBigNumbersLoop() {
-		for (int i = 0; i < 1000; i++)
-			substractTwoBigNumbers();
-	}
-
-	void multiplyTwoBigNumbers() {
-		String aString = "", bString = "";
-		Random random = new Random();
-
-		for (int i = 0; i < random.nextInt(1000) + 1; i++)
-			aString += random.nextInt(10);
-
-		for (int i = 0; i < random.nextInt(1000) + 1; i++)
-			bString += random.nextInt(10);
-
-		BigInteger sum = new BigInteger(aString).multiply(new BigInteger(bString));
-		assertEquals(sum.toString(), new HyperInteger(aString).multiply(new HyperInteger(bString)).toString());
-	}
+        assertEquals(EXPECTED, underTest.toString());
+    }
 
 
+    @Test
+    void subtractShouldReturnDifference() {
+        final String EXPECTED = "122";
+        HyperInteger number1 = new HyperInteger("123");
+        HyperInteger number2 = new HyperInteger("1");
+        underTest = number1.subtract(number2);
 
-	@Test
-	void multiplyTwoBigNumbersLoop() {
-		int n = 100000;
-		for (int i = 0; i < n; i++) {
-			multiplyTwoBigNumbers();
-		}
-	}
+        assertEquals(EXPECTED, underTest.toString());
+    }
+
+    @Test
+    void subtractShouldReturnDifferenceWhenSubtractingBigNumbers() {
+        final String EXPECTED = "12345678901234567890";
+        HyperInteger number1 = new HyperInteger("111111111011111111100");
+        HyperInteger number2 = new HyperInteger("98765432109876543210");
+        underTest = number1.subtract(number2);
+
+        assertEquals(EXPECTED, underTest.toString());
+    }
+
+    @Test
+    void subtractShouldReturnDifferenceWhenSubtractingNegativeNumbers() {
+        final String EXPECTED = "3";
+        HyperInteger number1 = new HyperInteger("-2");
+        HyperInteger number2 = new HyperInteger("-5");
+        underTest = number1.subtract(number2);
+
+        assertEquals(EXPECTED, underTest.toString());
+    }
+
+    @Test
+    void subtractTwoBigNegativeNumbers() {
+        final String EXPECTED = "-12345678901234567890";
+        HyperInteger number1 = new HyperInteger("-111111111011111111100");
+        HyperInteger number2 = new HyperInteger("-98765432109876543210");
+        underTest = number1.subtract(number2);
+
+        assertEquals(EXPECTED, underTest.toString());
+    }
+
+    @Test
+    void multiplyShouldReturnProductWhenMultiplyingTwoPositiveNumbers() {
+        final String EXPECTED = "15129";
+        HyperInteger number1 = new HyperInteger("123");
+        HyperInteger number2 = new HyperInteger("123");
+        underTest = number1.multiply(number2);
+
+        assertEquals(EXPECTED, underTest.toString());
+    }
+
+    @Test
+        /*
+         * https://mae.ufl.edu/~uhk/QUICK-SEMI-PRIME-FACTORING.pdf
+         */
+    void multiplyShouldReturnProductWhenMultiplyingTwoReallyBigPositiveNumbers() {
+        final String EXPECTED = "194920496263521028482429080527";
+        HyperInteger number1 = new HyperInteger("289673451203483");
+        HyperInteger number2 = new HyperInteger("672897345109469");
+        underTest = number1.multiply(number2);
+
+        assertEquals(EXPECTED, underTest.toString());
+    }
+
+    @Test
+    void multiplyShouldReturnProductWhenMultiplyingTwoNegativeNumbers() {
+        final String EXPECTED = "15129";
+        HyperInteger number1 = new HyperInteger("-123");
+        HyperInteger number2 = new HyperInteger("-123");
+        underTest = number1.multiply(number2);
+
+        assertEquals(EXPECTED, underTest.toString());
+    }
+
+    @Test
+    void multiplyShouldReturnProductWhenMultiplyingOnePositiveOneNegativeNumber() {
+        final String EXPECTED = "-15129";
+        HyperInteger number1 = new HyperInteger("123");
+        HyperInteger number2 = new HyperInteger("-123");
+        underTest = number1.multiply(number2);
+
+        assertEquals(EXPECTED, underTest.toString());
+    }
+
+    @Test
+    void divideShouldReturnQuotientWhenDividingTwoPositiveNumbers() {
+        final String EXPECTED = "1";
+        HyperInteger number1 = new HyperInteger("123");
+        HyperInteger number2 = new HyperInteger("123");
+        try {
+            underTest = number1.divide(number2);
+            assertEquals(EXPECTED, underTest.toString());
+        } catch (ArithmeticException e) {
+            fail("Division by zero");
+        }
+    }
+
+    @Test
+        /*
+         * https://mae.ufl.edu/~uhk/QUICK-SEMI-PRIME-FACTORING.pdf
+         */
+    void divideShouldReturnQuotientWhenDividingTwoBigPositiveNumbers() {
+        final String EXPECTED = "672897345109469";
+        HyperInteger number1 = new HyperInteger("194920496263521028482429080527");
+        HyperInteger number2 = new HyperInteger("289673451203483");
+        try {
+            underTest = number1.divide(number2);
+            assertEquals(EXPECTED, underTest.toString());
+        } catch (ArithmeticException e) {
+            fail("Division by zero");
+        }
+    }
+
+    @Test
+    void divideShouldThrowArithmeticExceptionWhenDividingByZero() {
+        final Class<? extends ArithmeticException> EXPECTED = ArithmeticException.class;
+        final String EXCEPTION_MESSAGE = "Division by 0";
+
+        HyperInteger number1 = new HyperInteger("123");
+        HyperInteger number2 = new HyperInteger("0");
+
+        ArithmeticException result = assertThrows(ArithmeticException.class, () -> number1.divide(number2));
+
+        assertThrows(EXPECTED, () -> number1.divide(number2));
+        assertEquals(EXCEPTION_MESSAGE, result.getMessage());
+    }
 }
