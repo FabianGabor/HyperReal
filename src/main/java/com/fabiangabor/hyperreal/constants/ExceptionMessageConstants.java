16e0dd570aa095708c98c8fa39d07ab03a0ed714
Fábián Gábor <gabor_fabian1@epam.com>
2022-04-01 13:51:12 +0200

    refactor constants

--- src/main/java/com/fabiangabor/hyperreal/constants/ExceptionMessageConstants.java
+++ src/main/java/com/fabiangabor/hyperreal/constants/ExceptionMessageConstants.java
@@ -6,17 +6,19 @@
 
 package com.fabiangabor.hyperreal.constants;
 
+import static com.fabiangabor.hyperreal.constants.NumberConstants.ZERO;
+
 public class ExceptionMessageConstants {
     public static final String MSG_UTILITY_CLASS = "Utility class";
     public static final String INVALID_NUMBER = "Invalid number";
-    public static final String DIVISION_BY_ZERO;
+    public static final String DIVISION_BY_ZERO = "Division by " + ZERO;
     public static final String NUMBERS_NOT_SUPPORTED = "is not supported for this type of numbers";
     public static final String ADDITION = "Addition";
     public static final String SUBTRACTION = "Subtraction";
     public static final String MULTIPLICATION = "Multiplication";
     public static final String DIVISION = "Division";
 
-    public ExceptionMessageConstants() {
-        this.DIVISION_BY_ZERO = "Division by " + NumberConstants.ZERO;
+    private ExceptionMessageConstants() {
+        throw new IllegalStateException(MSG_UTILITY_CLASS);
     }
 }
